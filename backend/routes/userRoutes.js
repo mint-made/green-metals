dc3f0fbecaf0268c2e864bbd026253d6200fcb07
mint-made <thomaskupai@gmail.com>
2021-06-23 21:36:24 +0100

    updateUserProfile HTTP PUT method added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,16 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from '../controllers/userContoller.js';
 
 const router = express.Router();
 
 router.route('/').post(registerUser);
-router.route('/profile').get(protect, getUserProfile);
 router.post('/login', authUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
