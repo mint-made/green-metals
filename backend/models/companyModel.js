90adaa5a154e126e6f76a4ecd9ac9ab39528117f
mint-made <thomaskupai@gmail.com>
2021-10-04 20:08:03 +0100

    data model for company assets

--- backend/models/companyModel.js
+++ backend/models/companyModel.js
@@ -6,15 +6,24 @@ const assetSchema = new mongoose.Schema({
     country: { type: String, required: true },
     province: { type: String },
     lat: { type: Number },
+    lng: { type: Number },
   },
   stage: { type: String, required: true },
+  ownership: { type: String, required: true },
+  study: { type: String, required: true },
   resource: [
     {
-      size: { type: Number },
+      inferred: { type: Number },
+      indicated: { type: Number },
+      measured: { type: Number },
       units: { type: String },
       commodity: { type: String },
     },
   ],
+  npv: {
+    value: { type: Number },
+    discount: { type: Number },
+  },
 });
 
 const companySchema = new mongoose.Schema({
