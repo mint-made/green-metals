1d6682b06b1fb6bf10167c00183fba445deac788
mint-made <thomaskupai@gmail.com>
2021-10-08 00:06:49 +0100

    get asset by ID route added

--- backend/controllers/assetController.js
+++ backend/controllers/assetController.js
@@ -2,12 +2,26 @@ import asyncHandler from 'express-async-handler';
 
 import Asset from '../models/assetModel.js';
 
-// @description Fetch all companies
-// @route GET /api/companies
+// @description Fetch all assets
+// @route GET /api/assets
 // @access Public
 const getAssets = asyncHandler(async (req, res) => {
   const assets = await Asset.find({});
   res.json(assets);
+});
+
+// @description Fetch a single asset
+// @route GET /api/assets/:id
+// @access Public
+const getAssetById = asyncHandler(async (req, res) => {
+  const asset = await Asset.findById(req.params.id);
+
+  if (asset) {
+    res.json(asset);
+  } else {
+    res.status(404);
+    throw new Error('Company not found');
+  }
 });
 
 // @description Create an Asset
@@ -27,4 +41,4 @@ const createAsset = asyncHandler(async (req, res) => {
   res.json(createdAsset);
 });
 
-export { createAsset, getAssets };
+export { createAsset, getAssets, getAssetById };
--- backend/routes/assetRoutes.js
+++ backend/routes/assetRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 
 import { isAdmin, protect } from '../middleware/authMiddleware.js';
-import { createAsset, getAssets } from '../controllers/assetController.js';
+import {
+  createAsset,
+  getAssets,
+  getAssetById,
+} from '../controllers/assetController.js';
 
 const router = express.Router();
 
 router.route('/').post(protect, isAdmin, createAsset).get(getAssets);
+router.route('/:id').get(getAssetById);
 
 export default router;
