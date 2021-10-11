231e69799c8bc36e73ce006c0e4b62d7e55fd0b8
mint-made <thomaskupai@gmail.com>
2021-10-11 23:17:24 +0100

    basic edit options added to assetEditScreen

--- frontend/src/screens/AssetEditScreen.js
+++ frontend/src/screens/AssetEditScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Col, Row, Button } from 'react-bootstrap';
+import { Col, Row, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { listAssetDetails } from '../actions/assetActions';
@@ -10,6 +10,9 @@ const AssetEditScreen = ({ history, match }) => {
   const dispatch = useDispatch();
 
   const assetId = match.params.id;
+  const [name, setName] = useState('');
+  const [stage, setStage] = useState('');
+  const [study, setStudy] = useState('');
 
   const assetDetails = useSelector((state) => state.assetDetails);
   const { loading, error, asset } = assetDetails;
@@ -26,14 +29,78 @@ const AssetEditScreen = ({ history, match }) => {
 
     if (!asset.name) {
       dispatch(listAssetDetails(assetId));
+    } else {
+      setName(asset.name);
+      setStage(asset.stage);
+      setStudy(asset.study);
     }
   }, [history, userInfo]);
+
+  const submitHandler = () => {
+    console.log('Form submitted');
+  };
 
   return (
     <>
       <Row>
-        <Col sm={3} md={4}></Col>
-        <Col sm={9} md={8}></Col>
+        <Col sm={3} md={4}>
+          <Form.Group controlId='logo'>
+            <Form.Label>AssetId</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='assetId'
+              value={assetId}
+              onChange={(e) => console.log(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+        </Col>
+        <Col sm={9} md={8}>
+          <Form onSubmit={submitHandler}>
+            <Row>
+              <Col>
+                <Form.Group controlId='name'>
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    type='name'
+                    placeholder='Enter Name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  ></Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId='stage'>
+                  <Form.Label>Stage</Form.Label>
+                  <Form.Control
+                    type='name'
+                    placeholder='Enter Website'
+                    value={stage}
+                    onChange={(e) => setStage(e.target.value)}
+                  ></Form.Control>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group controlId='Study'>
+                  <Form.Label>Study</Form.Label>
+                  <Form.Control
+                    as='select'
+                    value={study}
+                    onChange={(e) => {
+                      setStudy(e.target.value);
+                    }}
+                  >
+                    <option value='-'>-</option>
+                    <option value='PFS'>PFS</option>
+                    <option value='DFS'>DFS</option>
+                    <option value='BFS'>BFS</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
       </Row>
       <Row>
         <Col></Col>
