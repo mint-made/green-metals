6f0e43042b59905feaa1277c135c2f5d51e50339
mint-made <thomaskupai@gmail.com>
2021-10-23 11:02:43 +0100

    assetSummary updated with links to the assetScreen for each asset

--- frontend/src/components/AssetSummary.js
+++ frontend/src/components/AssetSummary.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { Table, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const AssetSummary = ({ asset }) => {
   return (
     <Card className='mb-1 rounded pt-3'>
       <Card.Title>
-        <h4 className='text-center mb-0'>{asset.name}</h4>
+        <Link to={`/asset/${asset.assetRef}`}>
+          <div className=' '>
+            <h5 className='text-center mb-0'>
+              {asset.name}
+              <span>
+                <i className='pl-1 fas fa-info-circle text-info'></i>
+              </span>
+            </h5>
+          </div>
+        </Link>
       </Card.Title>
 
       <Card.Body className='pt-0'>
         <Table className='mb-0' size='sm'>
           <tbody>
             <tr>
-              <td>Location</td>
-              <td>{asset.location}</td>
+              <td>Stake Percent</td>
+              <td>{asset.stakePercent}%</td>
             </tr>
             <tr>
-              <td>Stage</td>
-              <td>{asset.stage}</td>
-            </tr>
-            <tr>
-              <td>Metals</td>
-              <td>
-                {asset.resource
-                  .map((resource) => {
-                    return resource.commodity;
-                  })
-                  .join(', ')}
-              </td>
+              <td>Asset Ref</td>
+              <td>{asset.assetRef}</td>
             </tr>
           </tbody>
         </Table>
